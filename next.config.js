f5cd1f34e66baa04ecdabf734318d79fb5efba3d
Ossamoon <73047429+Ossamoon@users.noreply.github.com>
2023-08-24 02:40:11 +0900

    change next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     appDir: true,
   },
